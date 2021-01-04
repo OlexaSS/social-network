aea2bc98c03a561e788423dd515273234cf6a573
Пользователь <ssolexa@gmail.com>
2021-01-04 13:53:32 +0200

    page create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
+import './reset.css';
 import './App.css';
-import Footer from "./Footer";
-import Header from './Header.js';
+import image from './main-img.jpg';
+
 
 const App = () => {
 	return (
-		<div className="App">
-			<Header />
-			<Footer />
+		<div className="app-wrapper">
+			<header className="header">
+				<div className="logo-wrapper">Logo</div>
+				<div className="header-description">Socialnet</div>
+			</header>
+			<nav className="navbar">
+				<ul className="navbar__list">
+					<li className="navbar__list-item"><a href="#">Profile</a></li>
+					<li className="navbar__list-item"><a href="#">Messages</a></li>
+					<li className="navbar__list-item"><a href="#">News</a></li>
+					<li className="navbar__list-item"><a href="#">Music</a></li>
+					<li className="navbar__list-item"><a href="#">Settings</a></li>
+				</ul>
+			</nav>
+			<div className="content">
+				<div className="img-wrapper">
+					<img className="profile-img" src={image} />
+				</div>
+				<div className="content-inner">
+					<div className="content-ava">ava</div>
+					<div className="content-descr">descr</div>
+				</div>
+				<div className="posts">
+					My posts
+					<div className="new-post">
+						new post
+					</div>
+					<div>
+						post 1
+					</div>
+					<div>
+						post 2
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
