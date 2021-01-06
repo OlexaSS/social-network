f0d56347b263f32bfbaec22fc1c75c469e256760
Пользователь <ssolexa@gmail.com>
2021-01-06 11:02:04 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/dialogs/Dialogs";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header />
-			<Navbar />
-			<div className="wrapper__content">
-				<Dialogs />
-				{/* <Profile /> */}
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header />
+				<Navbar />
+				<div className="wrapper__content">
+					<Route path='/dialogs' component={Dialogs} />
+					<Route path='/profile' component={Profile} />
+				</div>
 			</div>
-			
-
-		</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <nav className={s.navbar}>
             <ul className={s.list}>
-                <li className={`${s.list__item} ${s.active}`}><a href="/">Profile</a></li>
-                <li className={s.list__item}><a href="/">Messages</a></li>
+                <li className={`${s.list__item} ${s.active}`}><a href="/profile">Profile</a></li>
+                <li className={s.list__item}><a href="/dialogs">Messages</a></li>
                 <li className={s.list__item}><a href="/">News</a></li>
                 <li className={s.list__item}><a href="/">Music</a></li>
                 <li className={s.list__item}><a href="/">Settings</a></li>
