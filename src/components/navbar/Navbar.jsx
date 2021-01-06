fd5bd1716a7af551fec2b998674904e8c2d8e781
Пользователь <ssolexa@gmail.com>
2021-01-06 12:01:23 +0200

    NavLink class active

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
         <nav className={s.navbar}>
             <ul className={s.list}>
-                <li className={`${s.list__item} ${s.active}`}><a href="/profile">Profile</a></li>
-                <li className={s.list__item}><a href="/dialogs">Messages</a></li>
-                <li className={s.list__item}><a href="/news">News</a></li>
-                <li className={s.list__item}><a href="/music">Music</a></li>
-                <li className={s.list__item}><a href="/settings">Settings</a></li>
+                <li className={s.list__item}><NavLink activeClassName={s.active} to="/profile">Profile</NavLink></li>
+                <li className={s.list__item}><NavLink activeClassName={s.active}  to="/dialogs">Messages</NavLink></li>
+                <li className={s.list__item}><NavLink activeClassName={s.active} to="/news">News</NavLink></li>
+                <li className={s.list__item}><NavLink activeClassName={s.active} to="/music">Music</NavLink></li>
+                <li className={s.list__item}><NavLink activeClassName={s.active} to="/settings">Settings</NavLink></li>
             </ul>
         </nav>
     );
