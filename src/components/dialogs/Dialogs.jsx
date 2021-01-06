21c6d70de4a26496824ab12c6e26de8ea12eee92
Пользователь <ssolexa@gmail.com>
2021-01-06 12:34:41 +0200

    dialogs

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs__items}>
+                <div className={s.item + ' ' + s.active}>
+                    Olexa
+                </div>
+                <div className={s.item}>
+                    Vadim
+                </div>
+                <div className={s.item}>
+                    Vasya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 }
